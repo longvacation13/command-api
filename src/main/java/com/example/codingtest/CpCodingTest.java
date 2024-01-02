45f992fb800ca6dcee484ce12625c3e805c68845
superukie <superukie@naver.com>
2024-01-02 21:43:55 +0900

    StringToInteger

--- src/main/java/com/example/codingtest/CpCodingTest.java
+++ src/main/java/com/example/codingtest/CpCodingTest.java
@@ -9,9 +9,7 @@ public class CpCodingTest {
     public void test() {
         HashMap<String, String> map = new HashMap<>();
 
-        String[] scores = {"DS7541 A0", "CA0055 D+", "AI5543 C0", "OS1808 B-", "DS7541 B+", "AI0001 F", "DB0001 B-", "AI5543 D+",
-                           "DS7651 A+", "OS1808 B-"
-        };
+        String[] scores = {"dddd"};
 
         System.out.println(scores);
         
