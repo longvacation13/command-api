e62689524232ee067d1754d58de4f81513729d81
superukie <superukie@naver.com>
2024-01-29 19:36:25 +0900

    - ExceptionHandler  - ResponseStatus 추가

--- src/main/java/com/config/exception/CustomExceptionHandler.java
+++ src/main/java/com/config/exception/CustomExceptionHandler.java
@@ -9,9 +9,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.logging.LogManager;
@@ -27,6 +25,7 @@ public class CustomExceptionHandler {
 
     // 예외 메시지와 HTTP 상태 코드를 클라이언트에게 반환합니다.
     @ExceptionHandler(CustomException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseDto<CommResDto> handleCustomException(CustomException ex) {
 
         // log
@@ -49,6 +48,7 @@ public class CustomExceptionHandler {
     }
 
     @ExceptionHandler({MethodArgumentNotValidException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseDto<CommResDto> handleException(MethodArgumentNotValidException ex) {
 
         CommResDto<String> commResDto
